836d62a55fb4fe5d915a820031cb099c1734615b
Yuri Smirnov <tycooon@yandex.ru>
2021-06-04 18:25:00 +0300

    handle gem repush error

--- main.rb
+++ main.rb
@@ -13,5 +13,10 @@ Dir.glob("*.gemspec").each do |gemspec_file|
   gemspec = Gem::Specification.load(gemspec_file)
   gem_file = gemspec.full_name + ".gem"
   system!("gem", "build", gemspec_file)
-  system!("gem", "push", gem_file)
+
+  begin
+    system!("gem", "push", gem_file)
+  rescue => error
+    raise unless error.message.include?("Repushing of gem versions is not allowed")
+  end
 end
